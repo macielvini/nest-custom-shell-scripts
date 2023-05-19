f0ae8785038ff010361a18b85290a84ad3e40a5d
Vinicius Maciel <80479473+macielvini@users.noreply.github.com>
2023-05-19 00:43:36 -0300

    feat: create nest module

---
+++ nest_module.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# HOW TO RUN
+# ./nestmodule <MODULE_NAME>
+
+mkfile() { mkdir -p -- "$1" && touch -- "$1"/"$2"; }
+
+nestmodule() {
+
+  name=$1
+  capitalized="$(tr '[:lower:]' '[:upper:]' <<<${name:0:1})${name:1}"
+  path=$(pwd)
+
+  if [[ ! -f "nest-cli.json" ]]; then
+    echo "WARNING: Please, run from your NestJS project root folder!"
+    return 2
+  fi
+
+  if [ $# -eq 0 ]; then
+    echo >&2 "WARNING: Provide a module name"
+    return 1
+  fi
+
+  cd "${path}/src" && mkdir -p -- $name && mkfile $name/controller index.ts && mkfile $name/services index.ts && mkfile $name/repositories index.ts && nest g mo $name --no-spec
+}
